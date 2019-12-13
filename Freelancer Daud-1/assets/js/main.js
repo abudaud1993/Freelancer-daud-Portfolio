19fd3f6f1e059f5f34f4592613a0ede2af3a2ddf
abudaud1993 <abudaud1993@gmail.com>
2019-12-13 21:15:38 +0600

    Background Image added

--- Freelancer Daud-1/assets/js/main.js
+++ Freelancer Daud-1/assets/js/main.js
@@ -1,6 +1,17 @@
 (function($) {
 
 
+		 // data - background - img
+		 $("[data-background]").each(function () {
+			$(this).css(
+			  "background-image",
+			  "url(" + $(this).attr("data-background") + ")"
+			);
+		  });
+		
+		
+		
+		
 		// 	**============================
 		// Animated Headline 
 		// 	**==========================
